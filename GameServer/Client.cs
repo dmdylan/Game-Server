25c494ede9e2749ab724615f2492ef627f2004ae
dmdylan <dylandanowski@gmail.com>
2020-06-05 15:05:50 -0400

    Add project files.

---
+++ GameServer/Client.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Client
+    {
+        public static int dataBufferSize = 4096;
+        public int id;
+        public TCP tcp;
+        
+        public Client(int clientId)
+        {
+            id = clientId;
+            tcp = new TCP(id);
+        }
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+            private NetworkStream stream;
+            private byte[] receiveBuffer;
+
+            public TCP(int _id)
+            {
+                id = _id;
+            }
+
+            public void Connect(TcpClient _socket)
+            {
+                socket = _socket;
+                socket.ReceiveBufferSize = dataBufferSize;
+                socket.SendBufferSize = dataBufferSize;
+
+                stream = socket.GetStream();
+            }
+        }
+    }
+}
---
+++ GameServer/Server.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Server
+    {
+        public static int MaxPlayers { get; private set; }
+        public static int Port { get; private set; }
+
+        private static TcpListener tcpListener;
+
+        public void Start(int maxPlayers, int port)
+        {
+            MaxPlayers = maxPlayers;
+            Port = port;
+
+            Console.WriteLine("Starting server...");
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallBack), null);
+
+            Console.WriteLine($"Server started on {Port}.");
+        }
+
+        private static void TCPConnectCallBack(IAsyncResult result)
+        {
+            TcpClient client = tcpListener.EndAcceptTcpClient(result);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallBack), null);
+        }
+    }
+}
